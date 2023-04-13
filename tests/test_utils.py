7a033626fefad292643dfab634e4371033cd776c
Ather Abbas <ather_abbas786@yahoo.com>
2023-04-13 17:04:58 +0900

    updated utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,12 @@
 
-from ETUtil.ETUtil.utils import freq_in_mins_from_string
+import unittest
 
-for freq in ['1D', '20D', '5days',  'Daily', 'hourly', '5hours', '10H', '3Hour', 'min', 'minute',
+from ETUtil.utils import freq_in_mins_from_string
+
+for freq in ['1D', '20D', '5days',  'Daily', 'hourly',
+             '5hours', '10H', '3Hour', 'min', 'minute',
              '6min', '6mins', '10minutes',  'Hourly', '3Day']:
     print(freq, freq_in_mins_from_string(freq))
+
+if __name__ == "__main__":
+    unittest.main()
